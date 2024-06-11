ec5aec02b075c6f30adcb6f079a26c586daac2f8
DaniArias7 <129128034+DaniArias7@users.noreply.github.com>
2024-06-11 15:09:30 -0500

    initial

--- src/Controller/Controladortablas.py
+++ src/Controller/Controladortablas.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import psycopg2
+from psycopg2 import sql
 
 # Obtener la ruta del directorio actual del script
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -18,9 +19,8 @@ sys.path.append(".")
 
 
 # Importaciones
-from Model.MonthlyPaymentLogic import *
-import Model.MonthlyPaymentLogic as mp
-import Model.TablesEmployer as Temployer
+from Model import MonthlyPaymentLogic as mp
+from Model import TablesEmployer as Temployer
 import securitydb as st
 
 
@@ -38,21 +38,24 @@ class WorkersIncomeData:
         """Creates the user table in the database"""
         try:
             cursor = cls.GetCursor()
-            cursor.execute("""CREATE TABLE Employerinput(
-                        name varchar(300) NOT NULL,
-                        id varchar(300) PRIMARY KEY NOT NULL,
-                        basic_salary float NOT NULL, 
-                        monthly_worked_days int NOT NULL, 
-                        days_leave int NOT NULL, 
-                        transportation_allowance float NOT NULL,
-                        daytime_overtime_hours int NOT NULL, 
-                        nighttime_overtime_hours int NOT NULL, 
-                        daytime_holiday_overtime_hours int NOT NULL,
-                        nighttime_holiday_overtime_hours int NOT NULL, 
-                        sick_leave_days int NOT NULL, 
-                        health_contribution_percentage float NOT NULL,
-                        pension_contribution_percentage float NOT NULL, 
-                        solidarity_pension_fund_contribution_percentage float NOT NULL);""")
+            cursor.execute("""
+                CREATE TABLE Employerinput(
+                    name VARCHAR(300) NOT NULL,
+                    id VARCHAR(300) PRIMARY KEY NOT NULL,
+                    basic_salary FLOAT NOT NULL,
+                    monthly_worked_days INT NOT NULL,
+                    days_leave INT NOT NULL,
+                    transportation_allowance FLOAT NOT NULL,
+                    daytime_overtime_hours INT NOT NULL,
+                    nighttime_overtime_hours INT NOT NULL,
+                    daytime_holiday_overtime_hours INT NOT NULL,
+                    nighttime_holiday_overtime_hours INT NOT NULL,
+                    sick_leave_days INT NOT NULL,
+                    health_contribution_percentage FLOAT NOT NULL,
+                    pension_contribution_percentage FLOAT NOT NULL,
+                    solidarity_pension_fund_contribution_percentage FLOAT NOT NULL
+                );
+            """)
             cursor.connection.commit()
         except psycopg2.Error as e:
             print(f"Error creating table: {e}")
@@ -68,7 +71,7 @@ class WorkersIncomeData:
         """Drop the 'Employerinput' table if it exists in the database."""
         try:
             cursor = cls.GetCursor()
-            cursor.execute("""DROP TABLE Employerinput""")
+            cursor.execute("DROP TABLE IF EXISTS Employerinput")
             cursor.connection.commit()
         except psycopg2.Error as e:
             print(f"Error dropping table: {e}")
@@ -86,17 +89,17 @@ class WorkersIncomeData:
             cursor = cls.GetCursor()
             Temployer.Employerinput.primary_key(EMPLOYER.name, EMPLOYER.id, WorkersIncomeData)
             Temployer.Employerinput.notexist(EMPLOYER)
-            cursor.execute(f"""INSERT INTO Employerinput (name, id, basic_salary, monthly_worked_days, 
-                                days_leave, transportation_allowance, daytime_overtime_hours, nighttime_overtime_hours, 
-                                daytime_holiday_overtime_hours, nighttime_holiday_overtime_hours,
-                                sick_leave_days, health_contribution_percentage, pension_contribution_percentage, 
-                                solidarity_pension_fund_contribution_percentage)
-                                VALUES 
-                                ('{EMPLOYER.name}', '{EMPLOYER.id}', {EMPLOYER.basic_salary}, {EMPLOYER.monthly_worked_days}, 
-                                {EMPLOYER.days_leave}, {EMPLOYER.transportation_allowance}, {EMPLOYER.daytime_overtime_hours}, {EMPLOYER.nighttime_overtime_hours}, 
-                                {EMPLOYER.daytime_holiday_overtime_hours}, {EMPLOYER.nighttime_holiday_overtime_hours},
-                                {EMPLOYER.sick_leave_days}, {EMPLOYER.health_contribution_percentage}, {EMPLOYER.pension_contribution_percentage},
-                                {EMPLOYER.solidarity_pension_fund_contribution_percentage});""")
+            cursor.execute(sql.SQL("""
+                INSERT INTO Employerinput (name, id, basic_salary, monthly_worked_days, days_leave, transportation_allowance, 
+                                           daytime_overtime_hours, nighttime_overtime_hours, daytime_holiday_overtime_hours, 
+                                           nighttime_holiday_overtime_hours, sick_leave_days, health_contribution_percentage, 
+                                           pension_contribution_percentage, solidarity_pension_fund_contribution_percentage)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """), (EMPLOYER.name, EMPLOYER.id, EMPLOYER.basic_salary, EMPLOYER.monthly_worked_days, EMPLOYER.days_leave, 
+                  EMPLOYER.transportation_allowance, EMPLOYER.daytime_overtime_hours, EMPLOYER.nighttime_overtime_hours, 
+                  EMPLOYER.daytime_holiday_overtime_hours, EMPLOYER.nighttime_holiday_overtime_hours, EMPLOYER.sick_leave_days, 
+                  EMPLOYER.health_contribution_percentage, EMPLOYER.pension_contribution_percentage, 
+                  EMPLOYER.solidarity_pension_fund_contribution_percentage))
             cursor.connection.commit()
         except Temployer.faileprimarykey as error_primary_key:
             print(f"Primary key error: {error_primary_key}")
@@ -119,7 +122,7 @@ class WorkersIncomeData:
         """Delete a worker from the 'Employerinput' table based on the provided name and ID."""
         try:
             cursor = cls.GetCursor()
-            cursor.execute(f"""DELETE FROM Employerinput WHERE name= '{NAME}' AND id='{ID}';""")
+            cursor.execute(sql.SQL("DELETE FROM Employerinput WHERE name=%s AND id=%s"), (NAME, ID))
             cursor.connection.commit()
         except psycopg2.Error as e:
             print(f"Error deleting worker: {e}")
@@ -136,9 +139,8 @@ class WorkersIncomeData:
         try:
             Temployer.Employerinput.valor_presente(KEYUPDATE)
             cursor = cls.GetCursor()
-            cursor.execute(f"""UPDATE Employerinput
-                            SET {KEYUPDATE} = {VALUEUPDATE}
-                            WHERE name= '{NAME}' AND id='{ID}';""")
+            cursor.execute(sql.SQL("UPDATE Employerinput SET {}=%s WHERE name=%s AND id=%s").format(sql.Identifier(KEYUPDATE)), 
+                           (VALUEUPDATE, NAME, ID))
             cursor.connection.commit()
         except Temployer.updatenotfount:
             pass
@@ -156,7 +158,7 @@ class WorkersIncomeData:
         """Query the data of a worker from the 'Employerinput' table based on the provided name and ID."""
         try:
             cursor = cls.GetCursor()
-            cursor.execute(f"""SELECT * FROM Employerinput WHERE NAME = '{NAME}' AND id = '{ID}';""")
+            cursor.execute(sql.SQL("SELECT * FROM Employerinput WHERE name=%s AND id=%s"), (NAME, ID))
             fila = cursor.fetchone()
         except psycopg2.Error as e:
             print(f"Error querying worker: {e}")
@@ -185,7 +187,7 @@ class WorkersIncomeData:
                                             pension_contribution_percentage=fila[12],
                                             solidarity_pension_fund_contribution_percentage=fila[13])
             return result
-        
+
 class WorkersoutputsData:
     
     @staticmethod
@@ -200,24 +202,27 @@ class WorkersoutputsData:
         """Creates the user table in the database"""
         try:
             cursor = cls.GetCursor()
-            cursor.execute("""CREATE TABLE Employeroutput(
-                        name varchar(300) NOT NULL,
-                        id varchar(300) PRIMARY KEY NOT NULL,
-                        basic_salary float NOT NULL, 
-                        workdays int NOT NULL, 
-                        sick_leave int NOT NULL, 
-                        transportation_aid float NOT NULL,
-                        dayshift_extra_hours int NOT NULL, 
-                        nightshift_extra_hours int NOT NULL, 
-                        dayshift_extra_hours_holidays int NOT NULL,
-                        nightshift_extra_hours_holidays int NOT NULL, 
-                        leave_days int NOT NULL, 
-                        percentage_health_insurance float NOT NULL,
-                        percentage_retirement_insurance float NOT NULL, 
-                        percentage_retirement_fund float NOT NULL, 
-                        devengado float NOT NULL,
-                        deducido float NOT NULL,  
-                        amounttopay float NOT NULL);""")
+            cursor.execute("""
+                CREATE TABLE Employeroutput(
+                    name VARCHAR(300) NOT NULL,
+                    id VARCHAR(300) PRIMARY KEY NOT NULL,
+                    basic_salary FLOAT NOT NULL,
+                    workdays INT NOT NULL,
+                    sick_leave INT NOT NULL,
+                    transportation_aid FLOAT NOT NULL,
+                    dayshift_extra_hours INT NOT NULL,
+                    nightshift_extra_hours INT NOT NULL,
+                    dayshift_extra_hours_holidays INT NOT NULL,
+                    nightshift_extra_hours_holidays INT NOT NULL,
+                    leave_days INT NOT NULL,
+                    percentage_health_insurance FLOAT NOT NULL,
+                    percentage_retirement_insurance FLOAT NOT NULL,
+                    percentage_retirement_fund FLOAT NOT NULL,
+                    devengado FLOAT NOT NULL,
+                    deducido FLOAT NOT NULL,
+                    amounttopay FLOAT NOT NULL
+                );
+            """)
             cursor.connection.commit()
         except psycopg2.Error as e:
             print(f"Error creating table: {e}")
@@ -233,7 +238,7 @@ class WorkersoutputsData:
         """Drop the 'Employeroutput' table if it exists in the database."""
         try:
             cursor = cls.GetCursor()
-            cursor.execute("""DROP TABLE Employeroutput""")
+            cursor.execute("DROP TABLE IF EXISTS Employeroutput")
             cursor.connection.commit()
         except psycopg2.Error as e:
             print(f"Error dropping table: {e}")
@@ -260,31 +265,44 @@ class WorkersoutputsData:
                 verificar_result_total = mp.SettlementParameters(employer[2], employer[3], employer[4], employer[5],
                                                                 employer[6], employer[7], employer[8], employer[9], employer[10],
                                                                 employer[11], employer[12], employer[13])
-                cursor.execute(
-                    f"""INSERT INTO Employeroutput (name, id, basic_salary, workdays, sick_leave, transportation_aid, 
-                                                    dayshift_extra_hours, nightshift_extra_hours, dayshift_extra_hours_holidays, 
-                                                    nightshift_extra_hours_holidays, leave_days, percentage_health_insurance,
-                                                    percentage_retirement_insurance, percentage_retirement_fund, devengado, deducido, amounttopay)
-                        
-                            SELECT Employerinput.name,
-                            Employerinput.id,
-                            {round(calculate_salary(employer[2], employer[3], employer[4], employer[10]), 2)}, --basic_salary
-                            Employerinput.monthly_worked_days,
-                            {round(calculate_leave(employer[2], employer[10]), 2)}, --Employerinput.days_leave
-                            {calculate_transportation_aid(employer[5], employer[2])}, --Employerinput.transportation_allowance 
-                            {round(calculate_extra_hours(employer[2], employer[6], mp.EXTRA_HOUR_DAYSHIFT), 2)}, --Employerinput.daytime_overtime_hours
-                            {round(calculate_extra_hours(employer[2], employer[7], mp.EXTRA_HOUR_NIGHTSHIFT), 2)}, --Employerinput.nighttime_overtime_hours
-                            {round(calculate_extra_hours(employer[2], employer[8], mp.EXTRA_HOUR_DAYSHIFT_HOLIDAYS), 2)}, --Employerinput.daytime_holiday_overtime_hours 
-                            {round(calculate_extra_hours(employer[2], employer[9], mp.EXTRA_HOUR_NIGHTSHIFT_HOLIDAYS), 2)}, --Employerinput.nighttime_holiday_overtime_hours
-                            {round(calculate_sick_leave(employer[2], employer[4]), 2)}, --Employerinput.sick_leave_days
-                            {round(calculate_health_insurance(employer[2], employer[11]), 2)}, --Employerinput.health_contribution_percentage
-                            {round(calculate_retirement_insurance(employer[2], employer[12]), 2)}, --Employerinput.pension_contribution_percentage
-                            {round(calculate_retirement_fund(employer[2], employer[13]), 2)}, --Employerinput.solidarity_pension_fund_contribution_percentage
-                            {round(calculate_accrued_values(verificar_result_total), 2)}, ---devengado 
-                            {round(calculate_deducted_values(verificar_result_total), 2)}, --deducido
-                            {round(mp.calculate_settlement(verificar_result_total), 2)} --amounttopay  
-                        
-                        FROM Employerinput WHERE name='{employer[0]}' AND id='{employer[1]}';""")
+                cursor.execute(sql.SQL("""
+                    INSERT INTO Employeroutput (name, id, basic_salary, workdays, sick_leave, transportation_aid, 
+                                                dayshift_extra_hours, nightshift_extra_hours, dayshift_extra_hours_holidays, 
+                                                nightshift_extra_hours_holidays, leave_days, percentage_health_insurance,
+                                                percentage_retirement_insurance, percentage_retirement_fund, devengado, deducido, amounttopay)
+                    
+                    SELECT name, id, 
+                           %s, -- basic_salary
+                           monthly_worked_days, 
+                           %s, -- leave_days
+                           %s, -- transportation_allowance 
+                           %s, -- daytime_overtime_hours
+                           %s, -- nighttime_overtime_hours
+                           %s, -- daytime_holiday_overtime_hours 
+                           %s, -- nighttime_holiday_overtime_hours
+                           %s, -- sick_leave_days
+                           %s, -- health_contribution_percentage
+                           %s, -- pension_contribution_percentage
+                           %s, -- solidarity_pension_fund_contribution_percentage
+                           %s, -- devengado 
+                           %s, -- deducido
+                           %s -- amounttopay 
+                    FROM Employerinput WHERE name=%s AND id=%s
+                """), (round(mp.calculate_salary(employer[2], employer[3], employer[4], employer[10]), 2), 
+                       round(mp.calculate_leave(employer[2], employer[10]), 2), 
+                       mp.calculate_transportation_aid(employer[5], employer[2]), 
+                       round(mp.calculate_extra_hours(employer[2], employer[6], mp.EXTRA_HOUR_DAYSHIFT), 2), 
+                       round(mp.calculate_extra_hours(employer[2], employer[7], mp.EXTRA_HOUR_NIGHTSHIFT), 2), 
+                       round(mp.calculate_extra_hours(employer[2], employer[8], mp.EXTRA_HOUR_DAYSHIFT_HOLIDAYS), 2), 
+                       round(mp.calculate_extra_hours(employer[2], employer[9], mp.EXTRA_HOUR_NIGHTSHIFT_HOLIDAYS), 2), 
+                       round(mp.calculate_sick_leave(employer[2], employer[4]), 2), 
+                       round(mp.calculate_health_insurance(employer[2], employer[11]), 2), 
+                       round(mp.calculate_retirement_insurance(employer[2], employer[12]), 2), 
+                       round(mp.calculate_retirement_fund(employer[2], employer[13]), 2), 
+                       round(mp.calculate_accrued_values(verificar_result_total), 2), 
+                       round(mp.calculate_deducted_values(verificar_result_total), 2), 
+                       round(mp.calculate_settlement(verificar_result_total), 2), 
+                       employer[0], employer[1]))
                 cursor.connection.commit()
         except psycopg2.Error as e:
             print(f"Error populating table: {e}")
@@ -300,7 +318,7 @@ class WorkersoutputsData:
         """Query the data of a worker from the 'Employeroutput' table based on the provided name and ID."""
         try:
             cursor = cls.GetCursor()
-            cursor.execute(f"""SELECT * FROM Employeroutput WHERE NAME = '{NAME}' AND id = '{ID}';""")
+            cursor.execute(sql.SQL("SELECT * FROM Employeroutput WHERE name=%s AND id=%s"), (NAME, ID))
             fila = cursor.fetchone()
         except psycopg2.Error as e:
             print(f"Error querying worker: {e}")
@@ -334,25 +352,21 @@ class WorkersoutputsData:
                                               amounttopay=fila[16])
             return result
 
-
-
-
-f"""
-0. name
-1. id
-2. basic_salary
-3. workdays
-4. sick_leave
-5. transportation_aid
-6. dayshift_extra_hours
-7. nightshift_extra_hours
-8. dayshift_extra_hours_holidays
-9. nightshift_extra_hours_holidays
-10. leave_days
-11. percentage_health_insurance
-12. percentage_retirement_insurance
-13. percentage_retirement_fund
-14. devengado
-15. deducido
-16. amounttopay
-"""
+# Esta es la lista de campos y sus índices correspondiente
+# 0. name
+# 1. id
+# 2. basic_salary
+# 3. workdays
+# 4. sick_leave
+# 5. transportation_aid
+# 6. dayshift_extra_hours
+# 7. nightshift_extra_hours
+# 8. dayshift_extra_hours_holidays
+# 9. nightshift_extra_hours_holidays
+# 10. leave_days
+# 11. percentage_health_insurance
+# 12. percentage_retirement_insurance
+# 13. percentage_retirement_fund
+# 14. devengado
+# 15. deducido
+# 16. amounttopay
