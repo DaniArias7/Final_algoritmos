148f28b064b5da74351ae81afc46997679b9034c
DaniArias7 <129128034+DaniArias7@users.noreply.github.com>
2024-06-11 14:42:57 -0500

    initial

--- securitydb.py
+++ securitydb.py
@@ -1,7 +1,7 @@
 #Ingrese sus datos de la base de datos
 
-PGHOST='ep-late-dew-a5zxdzl8.us-east-2.aws.neon.tech'
-PGDATABASE='neondb'
-PGUSER='neondb_owner'
-PGPASSWORD='Gl1LUdEphgR7'
+PGHOST=''
+PGDATABASE=''
+PGUSER=''
+PGPASSWORD=''
 PGPORT=5432
