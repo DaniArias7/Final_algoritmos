48874d7fc57fa3bdf18bd33d66ab141825d089ac
DaniArias7 <129128034+DaniArias7@users.noreply.github.com>
2024-06-11 15:16:42 -0500

    initial

--- src/Controller/tempCodeRunnerFile.py
+++ src/Controller/tempCodeRunnerFile.py
@@ -1,10 +1,18 @@
 import sys
 import os
 
-# Obtén la ruta del directorio actual del script
+# Obtener la ruta del directorio actual del script
 current_dir = os.path.dirname(os.path.abspath(__file__))
-# Agrega la ruta del directorio Model al sys.path
-sys.path.append("C:/Users/ACER/liquidador_nomina")
+# Obtener la ruta del directorio principal del proyecto
+project_dir = os.path.abspath(os.path.join(current_dir, ".."))
+# Obtener la ruta del directorio del modelo
+model_dir = os.path.join(project_dir, "Model")
+
+
+# Agregar la ruta del directorio principal del proyecto y del modelo al sys.path
+sys.path.append(project_dir)
+sys.path.append(model_dir)
 sys.path.append("./src")
-from src.Model.MonthlyPaymentLogic import *
-import src.Model.MonthlyPaymentLogic as mp
+sys.path.append(".")
+from Model.MonthlyPaymentLogic import *
+import Model.MonthlyPaymentLogic as mp
